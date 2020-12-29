d57e2dce3e9741653b5e7e644092551ee05f6358
Priyadarshini Acharjee <62319328+Priyadarshini-25@users.noreply.github.com>
2020-12-29 10:48:30 +0530

    Create variance.R

---
+++ variance.R
@@ -0,0 +1,12 @@
+# R program to illustrate 
+# variance of vector 
+  
+# Create example vector 
+x <- c(1, 2, 3, 4, 5, 6, 7)    
+  
+# Apply var function in R  
+var(x)                        
+  
+print(x) 
+
+# OUTPUT : [1] 4.667
