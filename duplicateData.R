d76e03b944206e27a9e376444f7c66de366736c6
Priyadarshini Acharjee <62319328+Priyadarshini-25@users.noreply.github.com>
2021-01-02 11:01:47 +0530

    Create duplicateData.R

---
+++ duplicateData.R
@@ -0,0 +1,12 @@
+---
+title: "R Syntax"
+output: html_notebook
+---
+```{r}
+25 * 4 + 9 / 3
+```
+```{r}
+data <- c(120,22,22,31,15,120)
+unique_vals <- unique(data)# deletes duplicate data
+print(unique_vals)
+```
