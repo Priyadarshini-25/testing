2223d9f830ee0f95cd9aaf035c1f970de5a654e0
Priyadarshini Acharjee <62319328+Priyadarshini-25@users.noreply.github.com>
2021-01-04 18:34:11 +0530

    Create StandardDeviation.R

---
+++ StandardDeviation.R
@@ -0,0 +1,13 @@
+# R program to illustrate 
+# SD of vector 
+  
+# Create example vector 
+x <- c(1, 2, 3, 4, 5, 6, 7)    
+  
+# calculating SD in R  
+sqrt(var(x))                        
+  
+print(x) 
+
+# OUTPUT : [1] 2.160247
+#          [1] 1 2 3 4 5 6 7
